3a0960cfdcae8508c33b46a9813aed1b12e9d88a
John Bjerke <bjerkeweb@gmail.com>
2020-02-14 13:57:45 -0500

    init

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+
+function App() {
+  return (
+    <h1>Hello</h1>
+  )
+}
+
+ReactDom.render( <App />, document.getElementById('root') );
