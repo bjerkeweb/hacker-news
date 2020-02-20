ae47633a3d65925f10dd505243a918e6b23dce82
bjerkeweb <bjerkeweb@gmail.com>
2020-02-20 09:06:13 -0500

    add link icon, logo

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -5,6 +5,7 @@ export default function Nav() {
   return (
     <div className="header-main">
       <ul className="main-nav">
+        <li class="logo"></li>
         <li>
           <NavLink exact to='/' className="nav-link" activeClassName="active">Top</NavLink>
         </li>
